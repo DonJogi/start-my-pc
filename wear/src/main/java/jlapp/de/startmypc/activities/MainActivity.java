695df33f2581b949108db32718bbfad147c6a111
Jürgen Landgraf <juergen.landgraf@mailbox.org>
2016-03-19 17:19:29 +0100

    Sync preferences via WearPrefs library

--- wear/src/main/java/jlapp/de/startmypc/activities/MainActivity.java
+++ wear/src/main/java/jlapp/de/startmypc/activities/MainActivity.java
@@ -3,13 +3,14 @@ package jlapp.de.startmypc.activities;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import de.jlapp.shared.MagicPacketAsyncTask;
 import jlapp.de.startmypc.R;
 
 public class MainActivity extends Activity {
-
-    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +20,29 @@ public class MainActivity extends Activity {
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
+
+                initViews();
+            }
+        });
+    }
+
+    private void initViews() {
+        View startButton = findViewById(R.id.btn_start_my_pc);
+        if (startButton == null) {
+            return;
+        }
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String macAddress = getSharedPreferences(getString(R.string.preferences_shared_with_wear), MODE_PRIVATE)
+                        .getString(
+                                getString(R.string.pref_mac_address_key),
+                                getString(R.string.pref_mac_address_summary_default));
+
+                Toast.makeText(MainActivity.this, "Sending MagicPacket to " + macAddress, Toast.LENGTH_SHORT).show();
+
+                MagicPacketAsyncTask.executeNewTask(macAddress, "192.168.178.255");
             }
         });
     }
