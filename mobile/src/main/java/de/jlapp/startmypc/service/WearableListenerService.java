e3d1804d226c631ed6c0811142e86909cdfa18a3
Jürgen Landgraf <juergen.landgraf@mailbox.org>
2016-11-20 14:55:05 +0100

    Migrate manifest intent-filter
    Remove BIND_LISTENER and use the more specific MESSAGE_RECEIVED.

--- mobile/src/main/java/de/jlapp/startmypc/service/WearableListenerService.java
+++ mobile/src/main/java/de/jlapp/startmypc/service/WearableListenerService.java
@@ -1,12 +1,8 @@
 package de.jlapp.startmypc.service;
 
-import android.util.Log;
-import android.widget.Toast;
-
 import com.google.android.gms.wearable.MessageEvent;
 
 import de.jlapp.shared.Constants;
-import de.jlapp.shared.MagicPacketAsyncTask;
 import de.jlapp.startmypc.facade.MagicPacketFacade;
 
 public class WearableListenerService extends com.google.android.gms.wearable.WearableListenerService {
@@ -14,8 +10,6 @@ public class WearableListenerService extends com.google.android.gms.wearable.Wea
     @Override
     public void onMessageReceived(MessageEvent messageEvent) {
         if (messageEvent.getPath().equalsIgnoreCase(Constants.START_ACTIVITY)) {
-            Log.d("WearableListenerService", "Received " + Constants.START_ACTIVITY);
-
             MagicPacketFacade.sendMagicPacket(this);
         } else {
             super.onMessageReceived(messageEvent);
